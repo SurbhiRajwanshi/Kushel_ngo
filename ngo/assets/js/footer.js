c0b56ee9c15a10184521d9b0cfacdc7fc43dbb8b
ankitSInghMCA <ankitsingh30102000@gmail.com>
2024-03-05 04:48:43 +0530

    this

---
+++ ngo/assets/js/footer.js
@@ -0,0 +1,78 @@
+document.write(`
+<footer class="footer-area overlay text-white pt-120 bgs-cover">
+<div class="container">
+    <div class="row justify-content-between">
+        <div class="col-lg-3 col-md-6 col-sm-8">
+            <div class="widget widget_about">
+                <div class="logo_footer mb-25">
+                    <a href="index.html"><img src="assets/img/Kds logo white.png" alt="img"></a>
+                </div>
+                <!-- <p>Wimply dummy text of the priatype industry orem Ipsum has Maecenas quis eros at ante lacinia
+                    efficitur.</p> -->
+                <div class="social-style-one pt-20">
+                    <a href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="flaticon-google-plus-logo"></i></a>
+                    <a href="#"><i class="flaticon-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+            <div class="widget widget_nav_menu">
+                <h5 class="widget-title">About</h5>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Donation</a></li>
+                    <li><a href="#">About us</a></li>
+                    <li><a href="#">Event</a></li>
+                    <li><a href="#">Features</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+            <div class="widget widget_nav_menu">
+                <h5 class="widget-title">Quick links</h5>
+                <ul>
+                    <li><a href="#">Causes</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">New campaign</a></li>
+                    <li><a href="#">Site map</a></li>
+                    <li><a href="#">Stories</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-2 col-md-3 col-sm-4 col-6">
+            <div class="widget widget_nav_menu">
+                <h5 class="widget-title">Explore</h5>
+                <ul>
+                    <li><a href="#">Donate</a></li>
+                    <li><a href="#">Campaigns</a></li>
+                    <li><a href="#">Fundraise</a></li>
+                    <li><a href="#">Volunteers</a></li>
+                    <li><a href="#">Sponsors</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-8">
+            <div class="widget widget_gallery">
+                <h4 class="widget-title">Photo Gallery</h4>
+                <div class="gallery-photos">
+                    <a href="assets/img/footer/gallery1.jpg"><img src="assets/img/footer/gallery1.jpg"
+                            alt="Gallery"></a>
+                    <a href="assets/img/footer/gallery2.webp"><img src="assets/img/footer/gallery2.webp"
+                            alt="Gallery"></a>
+                    <a href="assets/img/footer/gallery3.jpg"><img src="assets/img/footer/gallery3.jpg"
+                            alt="Gallery"></a>
+                    <a href="assets/img/footer/gallery4.webp"><img src="assets/img/footer/gallery4.webp"
+                            alt="Gallery"></a>
+                    <a href="assets/img/footer/gallery3.jpg"><img src="assets/img/footer/gallery3.jpg"
+                            alt="Gallery"></a>
+                    <a href="assets/img/footer/gallery6.webp"><img src="assets/img/footer/gallery6.webp"
+                            alt="Gallery"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</footer>
+`)
---
+++ ngo/assets/js/nav.js
@@ -0,0 +1,77 @@
+document.write(`
+<div class="navbar-top pt-15 pb-10 bgc-black navtop--one">
+        <div class="container">
+            <div class="navtop-inner">
+                <ul class="topbar-left">
+                    <a href="#"><li><span><i class="fas fa-paper-plane"></i></span>ngo@kusheldigi.com</li></a>
+                    <li><i class="flaticon-pin"></i> G-9, first Floor, Sector 63, Noida</li>
+                </ul>
+                <ul class="topbar-right">
+                    <li class="social-area">
+                        <span>Follow Us - </span>
+                        <a href=""><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-pinterest-p"></i></a>
+                        <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    
+    <nav class="navbar py-30 navbar--one navbar-area navbar-expand-lg">
+        <div class="container nav-container navbar-bg">
+            <div class="responsive-mobile-menu">
+                <button class="menu toggle-btn d-block d-lg-none" data-target="#Iitechie_main_menu"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="icon-left"></span>
+                    <span class="icon-right"></span>
+                </button>
+            </div>
+            <div class="logo">
+                <a href="index.html"><img src="assets/img/logo.6e5a744de20300d46cb2.png" alt="img"></a>
+            </div>
+            <div class="nav-right-part nav-right-part-mobile">
+                <a class="search-bar-btn" href="#">
+                    <i class="flaticon-magnifying-glass"></i>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse" id="Iitechie_main_menu">
+                <ul class="navbar-nav menu-open text-lg-end">
+                    <li class="menu-item-has-children">
+                        <a href="index.html">Home</a>
+                       
+                    </li>
+                
+                    <li class="menu-item-has-children">
+                        <a href="about.html">About</a>
+                      
+                    </li>
+
+                    <li class="menu-item-has-children">
+                        <a href="portfolio.html">Portfolio</a>
+                       
+                    </li>
+                    <li class="menu-item-has-children">
+                        <a href="events.html">Event</a>
+                       
+                    </li>
+                  
+
+                    <li class="menu-item-has-children">
+                        <a href="contact.html">Contact Us</a>
+                       
+                    </li>
+                </ul>
+            </div>
+
+            <div class="nav-right-part nav-right-part-desktop">
+                <!-- <a class="search-bar-btn" href="#">
+                    <i class="flaticon-magnifying-glass"></i>
+                </a>
+                -->
+                <a class="btn btn--style-two" href="donate.html">Join Us</a>
+            </div>
+        </div>
+    </nav>
+`);
